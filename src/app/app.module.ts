c3f3ac82b3b4f274ba61ffbde822c22fcccda7ad
João Junior <joaodcpjunior@gmail.com>
2021-08-26 15:41:30 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContatoComponent,
+    InicioComponent,
+    SobreComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
